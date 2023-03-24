a81b2308eeff88e132196a0b236d63976098d4cc
Khoa <ldkhoa.11402@gmail.com>
2023-03-24 11:03:50 +0700

    Split user and owner app

--- User App/app/src/main/java/com/example/sportbooking/UserTabActivity.kt
+++ User App/app/src/main/java/com/example/sportbooking/UserTabActivity.kt
@@ -3,9 +3,9 @@ package com.example.sportbooking
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class UserTabActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_user_tab)
     }
 }
