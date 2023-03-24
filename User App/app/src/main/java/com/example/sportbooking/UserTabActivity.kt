68ce801f1c3ee883ca39ed2684335e29aeb00656
Khoa <ldkhoa.11402@gmail.com>
2023-03-24 12:14:52 +0700

    add bottom navigation bar and scratch user tab

--- User App/app/src/main/java/com/example/sportbooking/UserTabActivity.kt
+++ User App/app/src/main/java/com/example/sportbooking/UserTabActivity.kt
@@ -1,11 +1,36 @@
 package com.example.sportbooking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.google.android.material.navigation.NavigationBarView
 
 class UserTabActivity : AppCompatActivity() {
+    lateinit var nav_bar: NavigationBarView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_tab)
+        nav_bar = findViewById(R.id.nav_bar)
+        navBarHandle(nav_bar)
+
+    }
+    fun navBarHandle(nav_bar: NavigationBarView){
+        nav_bar.selectedItemId = R.id.item_user
+        nav_bar.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.item_home->{
+                    startActivity(Intent(this,MainActivity::class.java))
+                    overridePendingTransition(0,0)
+                    true
+                }
+                R.id.item_user->{
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
     }
 }
