a2a3966e01d47093012559c558cd7779f28519d5
Khoa <ldkhoa.11402@gmail.com>
2023-03-24 16:11:41 +0700

    edit avatar by take picture from camera or pick from gallery

--- User App/app/src/main/java/com/example/sportbooking/EditInformationActivity.kt
+++ User App/app/src/main/java/com/example/sportbooking/EditInformationActivity.kt
@@ -1,19 +1,19 @@
 package com.example.sportbooking
 
-import android.media.Image
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.Spinner
+import android.provider.MediaStore
+import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.datepicker.MaterialDatePicker
+import de.hdodenhof.circleimageview.CircleImageView
 import java.text.SimpleDateFormat
-import java.time.format.DateTimeFormatter
 import java.util.*
+
 
 class EditInformationActivity : AppCompatActivity() {
     lateinit var changeImgBtn: ImageButton
@@ -21,7 +21,12 @@ class EditInformationActivity : AppCompatActivity() {
     lateinit var genderInput: Spinner
     lateinit var dobInput: EditText
     lateinit var phoneInput: EditText
-
+    lateinit var avatarIv: CircleImageView
+    lateinit var backBtn:ImageButton
+    companion object{
+        val PICK_IMAGE_REQUEST = 201
+        val TAKE_IMAGE_REQUEST = 202
+    }
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +38,15 @@ class EditInformationActivity : AppCompatActivity() {
         genderInput = findViewById(R.id.genderSpinner)
         dobInput = findViewById(R.id.dobInput)
         phoneInput = findViewById(R.id.phoneInput)
+        avatarIv = findViewById(R.id.avatarIv)
+        backBtn = findViewById(R.id.backBtn)
+        backBtn.setOnClickListener {
+            finish()
+        }
+        changeImgBtn.setOnClickListener {
+           showBottomSheetDialog()
 
+        }
         ArrayAdapter(this, android.R.layout.simple_spinner_item, genderList)
             .also { adapter ->
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -44,6 +57,7 @@ class EditInformationActivity : AppCompatActivity() {
                 MaterialDatePicker.Builder.datePicker()
                     .setTitleText("Select date")
                     .build()
+
             datePicker.show(supportFragmentManager, "tag")
             datePicker.addOnPositiveButtonClickListener {
                 val selectedDate = datePicker.selection
@@ -53,5 +67,43 @@ class EditInformationActivity : AppCompatActivity() {
             }
         }
 
+    }
+    fun pickImageGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+    fun takeImageCamera(){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, TAKE_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK){
+            avatarIv.setImageURI(data?.data)
+        }
+        if(requestCode == TAKE_IMAGE_REQUEST && resultCode == RESULT_OK){
+            val img = (data?.extras!!.get("data")) as Bitmap
+            avatarIv.setImageBitmap(img)
+        }
+    }
+    private fun showBottomSheetDialog() {
+        val view = layoutInflater.inflate(R.layout.modal_bottom_sheet_content, null)
+
+        val dialog = BottomSheetDialog(this)
+        dialog.setContentView(view)
+
+        val openGalleryButton = view.findViewById<Button>(R.id.pickGaleryBtn)
+        val openCameraBtn = view.findViewById<Button>(R.id.pickCameraBtn)
+        openGalleryButton.setOnClickListener {
+            pickImageGallery()
+            dialog.dismiss()
+        }
+        openCameraBtn.setOnClickListener {
+            takeImageCamera()
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
