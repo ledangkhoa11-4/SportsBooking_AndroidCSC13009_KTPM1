42ac6f4db011424df9f792874dea97675b4b21e3
nguyen tran dai duong <87960153+aduongdnai@users.noreply.github.com>
2023-03-27 16:32:13 +0700

    test sign in with google

--- Owner App/app/src/main/java/com/example/sportbooking_owner/MainActivity.kt
+++ Owner App/app/src/main/java/com/example/sportbooking_owner/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startActivity(Intent(this, NewInfoActivity::class.java))
+        //startActivity(Intent(this, NewInfoActivity::class.java))
+        startActivity(Intent(this, SignIn::class.java))
     }
 }
--- Owner App/app/src/main/java/com/example/sportbooking_owner/SignIn.kt
+++ Owner App/app/src/main/java/com/example/sportbooking_owner/SignIn.kt
@@ -1,11 +1,74 @@
 package com.example.sportbooking_owner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+
 
 class SignIn : AppCompatActivity() {
+    var signInBtn:Button?=null
+    var signInGGBtn:Button?=null
+    var signInFbBtn:Button?=null
+    var signUp:Button?=null
+    var mGoogleSignInClient:GoogleSignInClient?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        signInBtn=findViewById<Button>(R.id.SignInBtn)
+        signInGGBtn=findViewById<Button>(R.id.SignInGGBtn)
+        signInFbBtn=findViewById<Button>(R.id.SignInFbBtn)
+        signUp=findViewById<Button>(R.id.SignUpBtn)
+
+        //Sign Up
+        signUp!!.setOnClickListener {
+            startActivity(Intent(this,SignUp::class.java))
+        }
+        //Sign In
+
+        //Sign In with google
+//        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//            .requestEmail()
+//            .build()
+//        mGoogleSignInClient= GoogleSignIn.getClient(this,gso)
+//        val account=GoogleSignIn.getLastSignedInAccount(this)
+//        if(account!=null){
+//            //sign in success
+//        }
+
     }
+//    private fun signIn(){
+//        val signInIntent=mGoogleSignInClient!!.signInIntent
+//        startActivityForResult(signInIntent,1000)
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if(requestCode==1000){
+//            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+//            handleSignInResult(task)
+//        }
+//    }
+//
+//    private fun handleSignInResult(task: Task<GoogleSignInAccount>) {
+//        try {
+//            val account: GoogleSignInAccount = task.getResult(ApiException::class.java)
+//
+//            // Signed in successfully, show authenticated UI.
+//            //updateUI(account)
+//        } catch (e: ApiException) {
+//            // The ApiException status code indicates the detailed failure reason.
+//            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+//            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+//            //updateUI(null)
+//        }
+//    }
 }
