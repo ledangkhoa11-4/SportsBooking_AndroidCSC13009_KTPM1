9241084ff423414826b9564df1f6c90f5fa2d9a6
Khoa <ldkhoa.11402@gmail.com>
2023-03-30 18:53:02 +0700

    Update UX/UI: Open GG map/Phone call app when click location/phone number

--- User App/app/src/main/java/com/example/sportbooking/DetailCourtActivity.kt
+++ User App/app/src/main/java/com/example/sportbooking/DetailCourtActivity.kt
@@ -1,8 +1,12 @@
 package com.example.sportbooking
 
+import android.content.Intent
+import android.graphics.Paint
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.viewpager2.widget.MarginPageTransformer
 import androidx.viewpager2.widget.ViewPager2
 import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
@@ -10,6 +14,9 @@ import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
 class DetailCourtActivity : AppCompatActivity() {
     lateinit var viewPager:ViewPager2
     lateinit var dot:DotsIndicator
+
+    lateinit var courtLocation:TextView
+    lateinit var courtPhone:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_court)
@@ -28,5 +35,23 @@ class DetailCourtActivity : AppCompatActivity() {
         viewPager.setPageTransformer(MarginPageTransformer(37));
         viewPager.adapter = vpAdapter
         dot.attachTo(viewPager)
+
+
+        courtLocation = findViewById(R.id.courtLocation)
+        courtLocation.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+        courtPhone =  findViewById(R.id.courtPhone)
+
+        courtLocation.setOnClickListener {
+            val location =courtLocation.text.toString()
+            val uri = Uri.parse("geo:0,0?q=$location")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+        courtPhone.setOnClickListener {
+            val phoneNumber =courtPhone.text.toString()
+            val uri = Uri.parse("tel:$phoneNumber")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 }
