c9d536fac3c3c354fd233ebd80bd5175267345f5
Khoa <ldkhoa.11402@gmail.com>
2023-03-30 22:52:48 +0700

    Rounded corner list image in detail. Add dependency for Firebase

--- User App/app/src/main/java/com/example/sportbooking/MainActivity.kt
+++ User App/app/src/main/java/com/example/sportbooking/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.navigation.NavigationBarMenu
 import com.google.android.material.navigation.NavigationBarView
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     lateinit var nav_bar:NavigationBarView
@@ -15,6 +17,16 @@ class MainActivity : AppCompatActivity() {
 
         // startActivity(Intent(this,CalendarViewActivity::class.java))
         startActivity(Intent(this,HomeActivity::class.java))
+
+
+
+        /*TEst database*/
+
+//        // Write a message to the database
+//        val database = Firebase.database("https://sportbooking2-b3fa8-default-rtdb.asia-southeast1.firebasedatabase.app/")
+//        val myRef = database.getReference("message")
+//
+//        myRef.setValue("Hello, Khoa!")
     }
 
 }
