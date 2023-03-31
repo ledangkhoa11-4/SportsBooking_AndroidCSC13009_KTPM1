175daa169852599167295d168166be75921ece7e
Khoa <ldkhoa.11402@gmail.com>
2023-03-31 11:42:11 +0700

    Fix conflix on app User

--- User App/app/src/main/java/com/example/sportbooking/MainActivity.kt
+++ User App/app/src/main/java/com/example/sportbooking/MainActivity.kt
@@ -16,17 +16,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // startActivity(Intent(this,CalendarViewActivity::class.java))
-        startActivity(Intent(this,RatingActivity::class.java))
+        //startActivity(Intent(this,RatingActivity::class.java))
+        startActivity(Intent(this,HomeActivity::class.java))
+        //startActivity(Intent(this,SearchStadiumActivity::class.java))
+        //startActivity(Intent(this,CourtScheduleActivity::class.java))
+        //startActivity(Intent(this,CalendarViewActivity::class.java))
 
 
 
         /*TEst database*/
 
-//        // Write a message to the database
-//        val database = Firebase.database("https://sportbooking2-b3fa8-default-rtdb.asia-southeast1.firebasedatabase.app/")
-//        val myRef = database.getReference("message")
-//
-//        myRef.setValue("Hello, Khoa!")
+        // Write a message to the database
+        val database = Firebase.database("https://sportbooking2-b3fa8-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        val myRef = database.getReference("message")
+        myRef.setValue("Hello, ABC!")
     }
 
 }
