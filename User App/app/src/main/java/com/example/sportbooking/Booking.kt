81beddc2bbb921ac4b22d14f4a01e67be7bedff2
Teddy <cuongthinh2002@gmail.com>
2023-04-05 22:04:21 +0700

    Add more UI for Booking.kt

--- User App/app/src/main/java/com/example/sportbooking/Booking.kt
+++ User App/app/src/main/java/com/example/sportbooking/Booking.kt
@@ -1,14 +1,20 @@
 package com.example.sportbooking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 
 class Booking : AppCompatActivity() {
     private lateinit var dateButton: Button
     private lateinit var dateSelected: TextView
+    private lateinit var timeButton: Button
+    private lateinit var timeSelected: TextView
+    private lateinit var checkoutButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +22,9 @@ class Booking : AppCompatActivity() {
 
         dateButton = findViewById(R.id.pick_date_button)
         dateSelected = findViewById(R.id.show_selected_date)
+        timeButton = findViewById(R.id.pick_time_button)
+        timeSelected = findViewById(R.id.show_selected_time)
+        checkoutButton = findViewById(R.id.checkout_button)
 
         val materialDateBuilder: MaterialDatePicker.Builder<*> = MaterialDatePicker.Builder.datePicker()
 
@@ -28,6 +37,63 @@ class Booking : AppCompatActivity() {
 
         materialDatePicker.addOnPositiveButtonClickListener {
             dateSelected.text = materialDatePicker.headerText
+        }
+
+        timeButton.setOnClickListener {
+            val materialTimePicker: MaterialTimePicker = MaterialTimePicker.Builder()
+                .setTitleText("SELECT YOUR TIMING")
+
+                .setHour(12) // set the default hour for the dialog when the dialog opens
+
+                .setMinute(10) // set the default minute for the dialog when the dialog opens
+
+                .setTimeFormat(TimeFormat.CLOCK_12H) // set the time format according to the region
+                .build()
+
+            materialTimePicker.show(supportFragmentManager, "Booking")
+            materialTimePicker.addOnPositiveButtonClickListener {
+
+                val pickedHour: Int = materialTimePicker.hour
+                val pickedMinute: Int = materialTimePicker.minute
+
+                // check for single digit hour hour and minute and update TextView accordingly
+                val formattedTime: String = when {
+                    pickedHour > 12 -> {
+                        if (pickedMinute < 10) {
+                            "${materialTimePicker.hour - 12}:0${materialTimePicker.minute} pm"
+                        } else {
+                            "${materialTimePicker.hour - 12}:${materialTimePicker.minute} pm"
+                        }
+                    }
+                    pickedHour == 12 -> {
+                        if (pickedMinute < 10) {
+                            "${materialTimePicker.hour}:0${materialTimePicker.minute} pm"
+                        } else {
+                            "${materialTimePicker.hour}:${materialTimePicker.minute} pm"
+                        }
+                    }
+                    pickedHour == 0 -> {
+                        if (pickedMinute < 10) {
+                            "${materialTimePicker.hour + 12}:0${materialTimePicker.minute} am"
+                        } else {
+                            "${materialTimePicker.hour + 12}:${materialTimePicker.minute} am"
+                        }
+                    }
+                    else -> {
+                        if (pickedMinute < 10) {
+                            "${materialTimePicker.hour}:0${materialTimePicker.minute} am"
+                        } else {
+                            "${materialTimePicker.hour}:${materialTimePicker.minute} am"
+                        }
+                    }
+                }
+                timeSelected.text = formattedTime // update the preview TextView
+            }
+        }
+
+        checkoutButton.setOnClickListener {
+            val intent = Intent(this@Booking, Checkout::class.java)
+            startActivity(intent)
         }
     }
 }
